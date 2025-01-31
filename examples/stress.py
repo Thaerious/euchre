a4852ab813b03f14a6144c49d8bc846a88c02709
Ed Armstrong <ed@frar.ca>
2025-01-31 14:05:00 -0500

    stress test updated

--- examples/stress.py
+++ examples/stress.py
@@ -3,23 +3,25 @@ from euchre.bots import Bot
 from euchre.card import *
 from euchre.class_getter import *
 import random
+import time
 
 names = ["Player1", "Player2", "Player3", "Player4"]
 bot = Bot()
 
-# for i in range(5):
+start = time.time()
 
-random.shuffle(names)
-game = Game(names)
-game.input(None, "start")
+for i in range(250):
+    random.shuffle(names)
+    game = Game(names)
+    game.input(None, "start")
 
-while game.current_state != 0:
-    print(game.current_state)
-    if game.current_state == 6:
-        game.input(None, "continue", None)    
-    if game.current_state == 7:
-        game.input(None, "continue", None)    
+    while game.current_state != 0:
+        if game.current_state in [6, 7]:
+            game.input(None, "continue", None)    
+        else:
+            (action, data) = bot.decide(Snapshot(game, game.current_player.name))
+            game.input(game.current_player.name, action, data)
 
-    (action, data) = bot.decide(Snapshot(game, game.current_player.name))
-    game.input(game.current_player.name, action, data)
-    print(f"{game.score} {game.is_game_over()}")
+end = time.time()
+print(f"Elapsed time: {end - start} seconds")
+print(f"Average time: {(end - start) / 250} seconds")
