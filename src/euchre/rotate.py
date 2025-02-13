ade1e2a1255b2e7fb2fdf71174feeeb548ec42b4
Ed Armstrong <ed@frar.ca>
2025-02-13 09:27:54 -0500

    renamed rotateTo -> rotate_to

--- src/euchre/rotate.py
+++ src/euchre/rotate.py
@@ -4,7 +4,7 @@ def rotate(list):
     list.append(list.pop(0))
     return list
 
-def rotateTo(list, target):
+def rotate_to(list, target):
     """ repeatedly move the last item to the first until target is first"""    
     if not target in list: raise Exception("Can not rotate list, target not present.")
     while list[0] != target:
