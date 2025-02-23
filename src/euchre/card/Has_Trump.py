20bdc87aed7baa110da5bccc2c82acc073acd12a
Ed Armstrong <ed@frar.ca>
2025-02-23 17:21:08 -0500

    value trump

--- src/euchre/card/Has_Trump.py
+++ src/euchre/card/Has_Trump.py
@@ -3,19 +3,12 @@ from cffi import FFI
 
 class Has_Trump():
     def __init__(self):
-        self.ffi = FFI()
-        self.trump_ptr = self.ffi.new("int *", 0)
+        self._trump = None
 
     @property
     def trump(self):
-        i = self.trump_ptr[0]
-        if i == -1: return None
-        return Card.suits[i]
+        return self._trump
 
     @trump.setter
     def trump(self, trump):
-        if trump is None:
-            self.trump_ptr = self.ffi.new("int *", -1)
-        else:
-            i = Card.suits.index(trump)
-            self.trump_ptr = self.ffi.new("int *", i)
+        self._trump = trump
